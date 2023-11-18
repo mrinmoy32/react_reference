e22ebd3f468f530b7b6e1bd7c08b62d80905a89b
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-11-18 21:46:29 +0530

    minor correction

--- src/components/context.js
+++ src/components/context.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export const myConstext=React.createContext();
-export const Provider = myConstext.Provider;
-export const Consumer = myConstext.Consumer;
+export const myContext=React.createContext();
+export const Provider = myContext.Provider;
+export const Consumer = myContext.Consumer;
