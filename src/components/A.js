6c7687a56c9bae6663d95d5ac915e79523abe548
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-11-18 22:28:09 +0530

    formatted

--- src/components/A.js
+++ src/components/A.js
@@ -1,38 +1,32 @@
-import React, { Component } from 'react'
-import B from './B'
+import React, { Component } from "react";
+import B from "./B";
 //export const myConstext = React.createContext();
-import { Provider } from './context'
+import { Provider } from "./context";
 
 export class A extends Component {
-    state = {
-        name: "Mrinmoy Pal"
-    }
-    learnContext = {
-        topic: "Context API",
-        // video:"40th"
-    }
+  state = {
+    name: "Mrinmoy Pal",
+  };
+  learnContext = {
+    topic: "Context API",
+    // video:"40th"
+  };
 
-    kalu = {
-        animal: "Cow"
-    }
+  kalu = {
+    animal: "Cow",
+  };
 
-
-
-    render(){
-
-        return (
-            <div>
-                
-                <Provider value={this.learnContext.topic}>
-                <B name={this.state.name} animal={this.kalu.animal}/> 
-                {/* <B name={this.state.name} animal={this.kalu.animal}></B>  I could have used this line direct also to avoid double use of <B/> once for context api and once for props */}
-                </Provider>
-                {/* <B name={this.state.name} animal={this.kalu.animal}></B> */}
-                
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Provider value={this.learnContext.topic}>
+          <B name={this.state.name} animal={this.kalu.animal} />
+          {/* <B name={this.state.name} animal={this.kalu.animal}></B>  I could have used this line direct also to avoid double use of <B/> once for context api and once for props */}
+        </Provider>
+        {/* <B name={this.state.name} animal={this.kalu.animal}></B> */}
+      </div>
+    );
+  }
 }
 
-export default A
+export default A;
